59c7a656557b3c55ac0e22654785c75e7705a40e
SREYA <sreya.virdikar@s.amity.edu>
2024-11-29 10:59:29 +0530

    Add files via upload

---
+++ PYTHON (Dictionary).py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 10:09:01 2024
+
+@author: sreya
+"""
+
+student= {"name": "alice","age": 21, "major": "computer science"}
+print(student)
+
+print(student["name"])
+
+student["age"] =22
+print(student)
+
+student["graduation_year"] = 2024
+print(student)
